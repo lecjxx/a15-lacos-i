f301736088daa27782813c0600503c7396b0985a
lecjxx <leticiacorreia2500@gmail.com>
2022-10-30 17:53:51 -0300

    aula de laços 1

---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+const series = ["Dark","Love, Drath and Roborts","Strager Things","Final Space", "Black Mirror"];
+
+
+console.log("Essas são mihas séries favoritas");
+i = 0
+
+for (let i = 0; i < series.length; i++) {
+    const element = series[i];
+    console.log(`Em ${i+1}º está a série: ${series[i]}`);
+}
